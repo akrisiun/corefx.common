76bc81e8f7c4c1abbac5d5779f2d0c55527e9e9a
AndriusK <akrisiun@gmail.com>
2016-11-12 12:06:12 +0200

    dotnet slow restore

--- build.cmd
+++ build.cmd
@@ -1,5 +1,8 @@
 @ECHO OFF
 
+@REM nuget restore Common.sln
+dotnet restore -v debug
+
 @REM PowerShell -NoProfile -NoLogo -ExecutionPolicy unrestricted -Command "[System.Threading.Thread]::CurrentThread.CurrentCulture = ''; [System.Threading.Thread]::CurrentThread.CurrentUICulture = '';& '%~dp0build.ps1' %*; exit $LASTEXITCODE"
 
 @set msbuild="%ProgramFiles(x86)%\MSBuild\14.0\Bin\MSBuild.exe"
@@ -7,6 +10,6 @@
 @if not exist %MSBuild% @set msbuild="%ProgramFiles(x86)%\MSBuild\12.0\Bin\MSBuild.exe"
 @if not exist %MSBuild% @set msbuild="%ProgramFiles%\MSBuild\12.0\Bin\MSBuild.exe"
 
-%msbuild% Sanitex.MvcHttp.sln  /p:Configuration=Debug /v:m Common.sln
+%msbuild% /p:Configuration=Debug /v:m Common.sln
 
 @PAUSE
